1a2f58f1c914bd699fc0bbbde4a8a5db99675fdb
jiyoung145900-png <jiyoung145900@gmail.com>
2025-12-20 00:32:43 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+function login() {
+  const id = document.getElementById('id').value;
+  const pw = document.getElementById('pw').value;
 
+  if (!id || !pw) {
+    alert('아이디와 비밀번호를 입력하세요');
+    return;
+  }
+
+  if (id === 'admin' && pw === '1234') {
+    alert('로그인 성공!');
+  } else {
+    alert('로그인 실패');
+  }
+}
